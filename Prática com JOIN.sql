a0c70d2ae2b8ae22b38856a78ab4a6bb3851e796
Januacele <januacelevieira@hotmail.com>
2022-07-26 22:34:15 -0300

    Ajustes nos exercícios + bônus

--- Prática com JOIN.sql
+++ Prática com JOIN.sql
@@ -1,18 +1,18 @@
 -- Questão 1:
 
-SELECT users.id, users.name, cities.name as city FROM users
+SELECT users.id, users.name, cities.name as city
+FROM users
 JOIN cities
-ON users.id = cities.id
+ON cities.id = users."cityId"
 WHERE cities.name = 'Rio de Janeiro'
-
-
 
 
 -- Questão 2: 
 
-SELECT users.name as writer, users.name as recipiente, testimonials.message as message FROM users
-JOIN testimonials
-ON users.id = testimonials.id
+SELECT t.id AS id, u1.name AS writer, u2.name AS recipient, message
+FROM testimonials t
+JOIN users u1 ON t."writerId" = u1.id
+JOIN users u2 ON t."recipientId" = u2.id;
 
 
 
@@ -36,13 +36,26 @@ WHERE users.id = 30
 
 -- Questão 4: 
 
-SELECT users.name as name, roles.name as role,
-companies.name as company, experiences."startDate" as "startDate"
-FROM users
-JOIN experiences 
-ON users.id = experiences."userId" AND experiences."endDate" = experiences."endDate"
-JOIN roles
-ON roles.id = experiences."roleId"
-JOIN companies
-ON companies.id = experiences."companyId"
-WHERE users.id = 50
+SELECT u.id AS id, u.name AS name, r.name AS role, c.name AS company, e."startDate" AS "startDate"
+FROM experiences e
+JOIN companies c ON e."companyId" = c.id
+JOIN roles r ON e."roleId" = r.id
+JOIN users u ON e."userId" = u.id
+WHERE u.id = 50
+AND e."endDate" IS NULL;
+
+
+-- Bônus
+
+SELECT s.id AS id, s.name AS school, c.name AS course, co.name AS company, r.name AS role
+FROM educations e
+JOIN schools s ON e."schoolId" = s.id
+JOIN courses c ON e."courseId" = c.id
+JOIN users u ON e."userId" = u.id
+JOIN applicants a ON a."userId" = u.id
+JOIN jobs j ON a."jobId" = j.id
+JOIN companies co ON j."companyId" = co.id
+JOIN roles r ON j."roleId" = r.id
+WHERE co.id = 10
+AND r.name = 'Software Engineer'
+AND j.active = true;
